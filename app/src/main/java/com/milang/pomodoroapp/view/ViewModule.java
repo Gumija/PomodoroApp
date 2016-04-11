d38705c87b3c971814f105d4ac5a70d2efce9d49
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-11 11:10:55 +0200

    default MVP sturcture

---
+++ app/src/main/java/com/milang/pomodoroapp/view/ViewModule.java
@@ -0,0 +1,32 @@
+package com.milang.pomodoroapp.view;
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+
+import com.milang.pomodoroapp.presenter.MainPresenter;
+/**
+ * Created by mobsoft on 2016. 04. 11..
+ */
+@Module
+public class ViewModule {
+    private Context  context;
+
+    public ViewModule(Context context) {
+        this.context = context;
+    }
+
+    @Provides
+    public Context provideContext() {
+        return context;
+    }
+
+    @Provides
+    @Singleton
+    public MainPresenter getMainPresenter() {
+        return new MainPresenter();
+    }
+}
