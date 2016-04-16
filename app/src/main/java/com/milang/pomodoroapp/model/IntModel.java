3961c795b8e41e0d7c610397df8e34f074ca2dd6
Milan Gulyas <gumija6@gmail.com>
2016-04-16 16:31:39 +0200

    Added boundary to random numbers. (0-100)

--- app/src/main/java/com/milang/pomodoroapp/model/IntModel.java
+++ app/src/main/java/com/milang/pomodoroapp/model/IntModel.java
@@ -1,13 +1,13 @@
 package com.milang.pomodoroapp.model;
 
 import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by mobsoft on 2016. 04. 11..
  */
 public class IntModel {
     public int getNextNum(){
-        Random r = new Random();
-        return r.nextInt();
+        return ThreadLocalRandom.current().nextInt(0, 100+1);
     }
 }
